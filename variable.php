206375ab131296b7ec08f9b1a0e5fdce5a539eb7
HwangSunTae <pozafly@gmail.com>
2021-06-20 14:28:59 +0900

    php

---
+++ variable.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<body>
+  <h1>Variable</h1>
+  <?php
+    $name = "pozafly";
+    echo "Lorem ipsum dolor ". $name ." sit amet 
+    consectetur adipisicing elit. Soluta iste 
+    molestias ea quisquam, voluptatibus, 
+    quo deleniti dolorem, adipisci 
+    labore aliquid ". $name ." ullam quos! 
+    ".$name." Odio natus ". $name ." accusamus 
+    itaque rerum optio earum assumenda".$name;
+  ?>
+</body>
+</html>
